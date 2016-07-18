1d0aa06e61196b7543c9f72ec92f8d0a950e0dee
Tatyana Korobitsina <Tatyana.Korobitsina@thomsonreuters.com>
2016-07-19 00:13:34 +0300

    add group

--- src/test/java/com/epam/tam/task4/BaseCalculatorTest.java
+++ src/test/java/com/epam/tam/task4/BaseCalculatorTest.java
@@ -1,15 +1,27 @@
 package com.epam.tam.task4;
 
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+
 import java.util.Date;
+
 import com.epam.tat.module4.Calculator;
 
 public class BaseCalculatorTest {
     protected Calculator calculator;
 
-    @BeforeClass
-    public void setUp() {
+    BaseCalculatorTest(){
         calculator = new Calculator();
+    }
+
+    @BeforeClass
+    public void bc() {
+        System.out.println("Before class");
+    }
+
+    @AfterClass
+    public void ac() {
+        System.out.println("After class");
     }
 
     protected void checkTime() {
